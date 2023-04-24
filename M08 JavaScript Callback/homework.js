5f4d2c0dfc83594d3f3c9348ced1f1a6229ff7a3
Alberto Mario Guerrero <psico.amgg@gmail.com>
2023-04-23 23:22:35 -0500

    Actualizar homework.js

--- M08 JavaScript Callback/homework.js
+++ M08 JavaScript Callback/homework.js
@@ -54,6 +54,16 @@ function filter(arrayOfStrings) {
   // Debes identificar todos los elementos el arreglo que comiencen con la letra "a".
   // Luego retorna un nuevo arreglo con estos elementos.
   // Tu código:
+newArray = []; 
+   
+   for (var i = 0; i < arrayOfStrings.length; i++) {
+   	if (arrayOfStrings[i][0] === letter ) {
+   		
+   		newArray.push(arrayOfStrings[i])
+   	} 	
+   }
+   return newArray
+ }
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
